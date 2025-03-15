a43d0067723f10b71bbc294d608921d5e0d77a76
Erickhub32 <erick.arevalolopez25@gmail.com>
2025-03-16 00:58:54 +0100

    modified:   config/index.js

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
+const cors = require("cors");
 const logger = require("morgan");
 const cookieParser = require("cookie-parser");
-const cors = require("cors");
 
 const FRONTEND_URL = process.env.ORIGIN || "http://localhost:3000";
 
